62177aa748ef18beef40502d3f532e5d2c1c760d
David Beck <david@rotundasoftware.com>
2014-03-20 03:10:51 -0300

    small bug fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function( srcFile, directoryMap ) {
 	var curDir = srcFile;
 	var srcDir = path.dirname( srcFile );
 
-	while( curDir !== '' ) {
+	while( curDir !== '' && curDir !== path.sep ) {
 		curDir = path.dirname( curDir );
 		var dstDir = typeof directoryMap === 'function' ? directoryMap( curDir ) : directoryMap[ curDir ];
 
